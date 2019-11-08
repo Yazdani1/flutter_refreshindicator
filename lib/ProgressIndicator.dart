acb6452ad5438e8f672f9463c7c666a6dbffb31f
Yazdani <yaz4dani@gmail.com>
2019-11-08 13:32:19 +0600

    Custom Bottom Bar

--- lib/ProgressIndicator.dart
+++ lib/ProgressIndicator.dart
@@ -2,20 +2,6 @@
 import 'package:flutter/material.dart';
 import 'package:progress_indicators/progress_indicators.dart';
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Progress Indicators',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Progress Indicators'),
-    );
-  }
-}
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,11 @@ import 'BubbleNavigationBar.dart';
 import 'SlideableDrawer.dart';
 import 'package:intro_slider/intro_slider.dart';
 import 'IntroSlider.dart';
+import 'ProgressIndicator.dart';
 
 void main(){
   runApp(new MaterialApp(
-    home: Introslider(),
+    home: MyHomePage(),
   ));
 }
 
